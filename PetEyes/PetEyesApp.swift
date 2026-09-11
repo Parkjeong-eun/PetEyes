3d40e8ca556a5e86ecd3a0cf4621ece9c7536b9e
Neeko <ddangddooongs@gmail.com>
2026-09-11 15:51:35 +0900

    Initial Commit

---
+++ PetEyes/PetEyesApp.swift
@@ -0,0 +1,17 @@
+//
+//  PetEyesApp.swift
+//  PetEyes
+//
+//  Created by 박정은 on 9/11/26.
+//
+
+import SwiftUI
+
+@main
+struct PetEyesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
